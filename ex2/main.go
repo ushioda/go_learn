7d8e89bbda73337c3b02580d3bef4115c245c847
Yu Ushioda <yuu1023@gmail.com>
2020-04-27 19:17:57 -0500

    exercise3

--- ex2/main.go
+++ ex2/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-var x int
-var y string
-var z bool
+var x int = 42
+var y string = "James Bond"
+var z bool = true
 
 func main() {
 	fmt.Printf("%v\n", x)
 	fmt.Printf("%v\n", y)
 	fmt.Printf("%v\n", z)
+	s := fmt.Sprintf("%v\t %v\t %v\t",x, y, z)
+	fmt.Println(s)
 }
