e6a81ef47e1c286d34aaf0993dff8dccd446498a
Yu Ushioda <yuu1023@gmail.com>
2020-04-28 20:26:56 -0500

    ascii loop

--- 008_loops/main.go
+++ 008_loops/main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for x := 0; x < 20; x++ {
-		if x%2 == 0 {
-			fmt.Println(x)
+	for x := 33; x < 123; x++ {
+		fmt.Printf("ASCII value %d corresponds to %#U\n", x, x)
 		}
 	}
-}
