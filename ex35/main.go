9e1f42f21a565b74cdd2ad8d6cf017032bc4849a
ushioda <yuu1023@gmaill.com>
2020-04-29 00:17:54 -0500

    exercise 3.7

--- ex35/main.go
+++ ex35/main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	for i := 10; i <= 100; i++{
-		if i%2 == 0{
+	for i := 10; i <= 100; i++ {
+		if i%2 == 0 {
 			fmt.Printf("The remainder of %d divided by 4 is %d \n", i, i%4)
+		} else if i == 10 {
+			fmt.Println("This is the first loop!")
+		} else {
+			fmt.Println("I'm an odd number, but not one!")
 		}
 	}
 }
-
