437274f8183e24fdcdcf7ecfae0c9a6a829b2e17
Yu Ushioda <yuu1023@gmail.com>
2020-04-29 13:53:07 -0500

    lecture 81 append

--- 010_groups/main.go
+++ 010_groups/main.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	x := []int{4, 5, 7, 8, 42}
 	fmt.Println(x)
-
-	for i := 0; i < len(x); i++ {
-		fmt.Println(x[i])
-	}
+	x = append(x, 77, 88, 99, 1014)
+	fmt.Println(x)
+	y := []int{234, 456, 678, 987}
+	x = append(x, y...)
+	fmt.Println(x)
 }
