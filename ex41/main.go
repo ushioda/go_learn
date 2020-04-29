38c1fa40086c8f722d7bd03e338a820761f697a8
Yu Ushioda <yuu1023@gmail.com>
2020-04-29 16:45:30 -0500

    exercise 4.3

--- ex41/main.go
+++ ex41/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	a := []int{23, 34, 23, 12, 55, 565, 324, 876, 45, 23}
-	for i, v := range a {
-		fmt.Println(i, v)
+	length := 5
+	for i := 0; i < len(a)-length+1; i++ {
+		fmt.Println(a[i : i+length])
 	}
-	fmt.Printf("%T \n", a)
 }
