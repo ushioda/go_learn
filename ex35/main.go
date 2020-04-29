f6d24665a1d80874a2618c4eda52217c45963ec2
ushioda <yuu1023@gmaill.com>
2020-04-29 00:12:46 -0500

    exercise 3.6

--- ex35/main.go
+++ ex35/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	for i := 10; i <= 100; i++{
-		fmt.Printf("The remainder of %d divided by 4 is %d \n", i, i%4)
+		if i%2 == 0{
+			fmt.Printf("The remainder of %d divided by 4 is %d \n", i, i%4)
+		}
 	}
 }
 
