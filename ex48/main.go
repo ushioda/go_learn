32072e25e97a3ae9cf8aee29ca99a01cb2bb1d8a
Yu Ushioda <yuu1023@gmail.com>
2020-04-29 17:42:36 -0500

    exercise 4.9

--- ex48/main.go
+++ ex48/main.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 	nl := map[string][]string{
 		"bond_james": {"shaken, not stirred", "martinis"},
-		"no_dr": {"being evil", "sunsets"},
+		"no_dr":      {"being evil", "sunsets"},
 	}
+
+	nl["man_spider"] = []string{"pizza", "beer"}
+
 	for key, p := range nl {
 		for i, item := range p {
 			fmt.Printf("%v likes %v, %d \n", key, item, i)
