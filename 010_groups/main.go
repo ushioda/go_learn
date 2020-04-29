dd9600a5b6b92347ed687576025c033eb7cb059d
Yu Ushioda <yuu1023@gmail.com>
2020-04-29 14:54:24 -0500

    lecture map and ok assertion

--- 010_groups/main.go
+++ 010_groups/main.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fr := []string{"James", "Bond", "Soccer"}
-	sr := []string{"Alice", "Smith", "Judo"}
-	fmt.Println(fr)
-	fmt.Println(sr)
-	xp := [][]string{fr, sr}
-	fmt.Println(xp)
+	m := map[string]int{
+		"James": 32,
+		"Bob":   40,
+	}
+	fmt.Println(m)
+	fmt.Println(m["James"])
+	fmt.Println(m["Bob"])
+	fmt.Println(m["Sam"])
+
+	v, ok := m["Sam"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	if _, ok := m["Sam"]; ok {
+		println("Sam exists")
+	} else {
+		println("Sam does not exist")
+	}
+
 }
