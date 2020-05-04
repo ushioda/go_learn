98d87f58eaedc648c646b7b30461e2d1d4e72c97
ushioda <yuu1023@gmaill.com>
2020-05-04 16:52:19 -0500

    interface

--- 014_functions/methods.go
+++ 014_functions/methods.go
@@ -13,7 +13,24 @@ type secretAgent struct {
 }
 
 func (sa secretAgent) speak() {
-	fmt.Println("I am", sa.first, sa.last)
+	fmt.Println("I am", sa.first, sa.last, "- secretAgent speaks")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, "- person speaks")
+}
+
+type human interface {
+	speak()
+}
+
+func bar(h human) {
+	switch h.(type) {
+	case secretAgent:
+		fmt.Println("I am a human, by", h.(secretAgent).first)
+	case person:
+		fmt.Println("I am a human, by", h.(person).first)
+	}
 }
 
 func main() {
@@ -31,7 +48,13 @@ func main() {
 		},
 		ltk: false,
 	}
-	fmt.Println(sa1)
+	p1 := person{
+		first: "Jimmy",
+		last:  "John",
+	}
 	sa1.speak()
 	sa2.speak()
+	bar(sa1)
+	bar(sa2)
+	bar(p1)
 }
