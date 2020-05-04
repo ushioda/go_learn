13cac4592cad743243198ee0d38c2443a76d407d
ushioda <yuu1023@gmaill.com>
2020-05-04 18:31:18 -0500

    callback

--- 014_functions/return_func.go
+++ 014_functions/return_func.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func ref() func() int {
-	return func() int {
-		return 451
+func sum2(xi ...int) int {
+	total := 0
+	for _, val := range xi {
+		total += val
 	}
+	return total
+}
+
+func even(f func(vi ...int) int, xi ...int) int {
+	var evens []int
+	for _, val := range xi {
+		if val%2 == 0 {
+			evens = append(evens, val)
+		}
+	}
+	return f(evens...)
 }
 
 func main() {
-	fmt.Println(ref()())
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(sum2(x...))
+	fmt.Println(even(sum2, x...))
 }
