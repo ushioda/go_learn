8ddfe892da17d081505ea0af4507a700198096f8
ushioda <yuu1023@gmaill.com>
2020-05-05 14:58:37 -0500

    exercise 7.2

--- ex71/main.go
+++ ex71/main.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
-var x int
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func (pa *person) changeMe(p person) {
+	(*pa).first = p.first
+	(*pa).age = p.age
+}
+
+func changeMe2(pa *person) {
+	(*pa).first = "Michael"
+	(*pa).age = 48
+}
 
 func main() {
-	x = 13
-	fmt.Println(&x)
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+	fmt.Println(p1)
+	p2 := person{
+		first: "Kim",
+		last:  "Philby",
+		age:   51,
+	}
+	p1.changeMe(p2)
+	fmt.Println(p1)
+	changeMe2(&p1)
+	fmt.Println(p1)
 }
