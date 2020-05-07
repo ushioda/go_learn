ea1fb11daf03731bca1cbe135ab5baa04b62bd96
ushioda <yuu1023@gmaill.com>
2020-05-06 21:43:56 -0500

    exercise 8.2

--- ex81/main.go
+++ ex81/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	json2 "encoding/json"
+	"encoding/json"
 	"fmt"
 )
 
@@ -32,9 +32,33 @@ func main() {
 
 	// your code goes here
 
-	bs, err := json2.Marshal(users)
+	bs, err := json.Marshal(users)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(bs))
+
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+
+	type user2 struct {
+		First   string
+		Last    string
+		Age     int
+		Sayings []string
+	}
+
+	var users2 []user2
+	err = json.Unmarshal([]byte(s), &users2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, v := range users2 {
+		fmt.Println(v.First, v.Last, v.Age)
+		fmt.Println("Favorite sayings are:")
+		for _, val := range v.Sayings {
+			fmt.Println("\t", val)
+		}
+		fmt.Println("-----------------")
+	}
 }
