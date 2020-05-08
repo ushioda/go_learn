da0bd795f4eaf761b5d85e47a09b5d133a9453dc
ushioda <yuu1023@gmaill.com>
2020-05-08 02:15:29 -0500

    exercise 9.4

--- ex93/main.go
+++ ex93/main.go
@@ -13,15 +13,19 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
+	var mu sync.Mutex
+
 	counter := 0
 	for i := 0; i < gs; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
 			runtime.Gosched()
 			v++
 			time.Sleep(time.Millisecond)
 			counter = v
 			fmt.Println("Counter value:", counter)
+			mu.Unlock()
 			wg.Done()
 		}()
 	}
