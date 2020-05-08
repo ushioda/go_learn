f17531d7025a0ac372157c6313750b84890f0f43
ushioda <yuu1023@gmaill.com>
2020-05-07 22:37:04 -0500

    mutex

--- 020_concurrency/race.go
+++ 020_concurrency/race.go
@@ -15,16 +15,20 @@ func main() {
 	counter := 0
 	const gs = 100
 
+	var mu sync.Mutex
+
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
 	for i := 0; i < gs; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
 			//time.Sleep(time.Millisecond)
 			runtime.Gosched()
 			v++
 			counter = v
+			mu.Unlock()
 			wg.Done()
 		}()
 		fmt.Println("GoRoutines:", runtime.NumGoroutine())
