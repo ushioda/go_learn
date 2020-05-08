e7bdb19471aff34723161c0d2eaa792065aa78a0
ushioda <yuu1023@gmaill.com>
2020-05-08 15:21:54 -0500

    directional channels

--- 022_channels/main.go
+++ 022_channels/main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 	c := make(chan int)
+	cr := make(<-chan int)
+	cs := make(chan<- int)
 
 	go func() {
 		c <- 42
 	}()
-	fmt.Println(<-c)
+	//fmt.Println(<-c)
+	fmt.Println("--------------")
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", cr)
+	fmt.Printf("%T\n", cs)
+
+	cr = c // from general to specific works
+	fmt.Println(<-cr)
 }
