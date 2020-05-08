cb67603a346cc9daba112b4afb5a91d97424ea82
ushioda <yuu1023@gmaill.com>
2020-05-07 22:59:32 -0500

    atomic

--- 020_concurrency/race.go
+++ 020_concurrency/race.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
+
 	//"time"
 )
 
@@ -12,23 +14,17 @@ func main() {
 	fmt.Println("CPUs:", runtime.NumCPU())
 	fmt.Println("GoRoutines:", runtime.NumGoroutine())
 
-	counter := 0
+	var counter int64
 	const gs = 100
-
-	var mu sync.Mutex
 
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
 	for i := 0; i < gs; i++ {
 		go func() {
-			mu.Lock()
-			v := counter
-			//time.Sleep(time.Millisecond)
+			atomic.AddInt64(&counter, 1)
 			runtime.Gosched()
-			v++
-			counter = v
-			mu.Unlock()
+			fmt.Println("Counter is", atomic.LoadInt64(&counter))
 			wg.Done()
 		}()
 		fmt.Println("GoRoutines:", runtime.NumGoroutine())
@@ -36,4 +32,5 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Counter is:", counter)
+
 }
