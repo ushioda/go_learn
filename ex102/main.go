d5efc9983c22f51061d61146b7e8abd23c0b3b00
Yu Ushioda <yuu1023@gmail.com>
2020-05-14 11:59:47 -0500

    exercise 10.2.2

--- ex102/main.go
+++ ex102/main.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	cs := make(chan int)
+	cr := make(chan int)
 
 	go func() {
-		cs <- 42
+		cr <- 42
 	}()
-	fmt.Println(<-cs)
+	fmt.Println(<-cr)
 
 	fmt.Printf("------\n")
-	fmt.Printf("cs\t%T\n", cs)
+	fmt.Printf("cr\t%T\n", cr)
 }
