9666208395fc18c3b6ecf21431e86548a59bc562
Yu Ushioda <yuu1023@gmail.com>
2020-05-14 15:19:30 -0500

    Todd's solution

--- ex106/main.go
+++ ex106/main.go
@@ -4,20 +4,13 @@ import "fmt"
 
 func main() {
 	ch := make(chan int)
-	closed := make(chan bool)
 	go func() {
 		for i := 0; i < 20; i++ {
 			ch <- i
 		}
 		close(ch)
-		closed <- true
 	}()
-	for {
-		select {
-		case i := <-ch:
-			fmt.Println(i)
-		case <-closed:
-			return
-		}
+	for i := range ch {
+		fmt.Println(i)
 	}
 }
