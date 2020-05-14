d1f492f17b753b0589c77c843e37811bb60c8f8f
ushioda <yuu1023@gmaill.com>
2020-05-14 00:44:37 -0500

    print context types

--- 022_channels/context.go
+++ 022_channels/context.go
@@ -10,4 +10,23 @@ func main() {
 	fmt.Println("context: \t", ctx)
 	fmt.Println("context error: \t", ctx.Err())
 	fmt.Printf("context type: \t %T\n", ctx)
+
+	fmt.Println("-------------------")
+
+	ctx, cancel := context.WithCancel(ctx)
+	fmt.Println("context: \t", ctx)
+	fmt.Println("context error: \t", ctx.Err())
+	fmt.Printf("context type: \t %T\n", ctx)
+	fmt.Println("cancel: \t", cancel)
+	fmt.Printf("cancel type: \t %T\n", cancel)
+
+	fmt.Println("-------------------")
+
+	cancel()
+
+	fmt.Println("context: \t", ctx)
+	fmt.Println("context error: \t", ctx.Err())
+	fmt.Printf("context type: \t %T\n", ctx)
+	fmt.Println("cancel: \t", cancel)
+	fmt.Printf("cancel type: \t %T\n", cancel)
 }
