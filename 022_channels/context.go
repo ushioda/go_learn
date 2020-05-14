ac21f234579f8e8694bc873c001adb745eeb830c
ushioda <yuu1023@gmaill.com>
2020-05-14 00:29:00 -0500

    context background

---
+++ 022_channels/context.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.Background()
+	fmt.Println("context: \t", ctx)
+	fmt.Println("context error: \t", ctx.Err())
+	fmt.Printf("context type: \t %T\n", ctx)
+}
