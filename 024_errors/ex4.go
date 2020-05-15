1138161761757e7c0d959795f72ba7fb17e45565
Yu Ushioda <yuu1023@gmail.com>
2020-05-15 14:17:32 -0500

    log file example

--- 024_errors/ex4.go
+++ 024_errors/ex4.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	//"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	_, err := os.Open("no-file.txt")
+
+	f, err := os.Create("log.txt")
+	if err != nil {
+		//fmt.Println("err happened", err)
+		log.Println(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	f2, err := os.Open("no-file.txt")
 	if err != nil {
 		//fmt.Println("err happened", err)
 		log.Println("err happened", err)
 	}
+	defer f2.Close()
+
+	fmt.Println("check the log.txt file in the directory")
+
 }
