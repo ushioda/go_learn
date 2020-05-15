3d6e52d18cb1cdc921d38b81421f00d9ad924a59
Yu Ushioda <yuu1023@gmail.com>
2020-05-15 14:12:48 -0500

    example 4

---
+++ 024_errors/ex4.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	//"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	_, err := os.Open("no-file.txt")
+	if err != nil {
+		//fmt.Println("err happened", err)
+		log.Println("err happened", err)
+	}
+}
