f66fd21d74541357a515cad3282cbe93bed74406
Yu Ushioda <yuu1023@gmail.com>
2020-05-15 15:05:09 -0500

    logging errors and panic

--- 024_errors/ex5.go
+++ 024_errors/ex5.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
 	//"fmt"
-	"log"
+	//"fmt"
+	//"log"
 	"os"
 )
 
 func main() {
-	defer foo()
+	//defer foo()
 	_, err := os.Open("no-file.txt")
 	if err != nil {
-		log.Fatalln(err)
+		//log.Fatalln(err)
+		//log.Panicln(err)
+		panic(err)
 	}
 }
 
-func foo() {
-	fmt.Println("When os.Exit() is called deferred function does not run")
-}
+//func foo() {
+//	fmt.Println("When os.Exit() is called deferred function does not run")
+//}
