86dc6d31a880b658c052f976635a37cd660eda78
ushioda <yuu1023@gmaill.com>
2020-05-16 02:12:40 -0500

    exercise 11.4

---
+++ ex114/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, sqrtError{
+			lat:  "50.2289 N",
+			long: "99.4656 W",
+			err:  errors.New("value you entered is negative"),
+		}
+	}
+	return 42, nil
+}
